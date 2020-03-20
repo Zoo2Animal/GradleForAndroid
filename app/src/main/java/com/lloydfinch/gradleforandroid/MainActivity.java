d78a7fb4942608737c87c3f18f202f2770054b66
LloydFinch <wangwenqi@orangelab.cn>
2020-03-20 11:22:31 +0800

    add viewbinding and java 1.8

--- app/src/main/java/com/lloydfinch/gradleforandroid/MainActivity.java
+++ app/src/main/java/com/lloydfinch/gradleforandroid/MainActivity.java
@@ -1,14 +1,56 @@
 package com.lloydfinch.gradleforandroid;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.lloydfinch.gradleforandroid.databinding.ActivityMainBinding;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnStart.setOnClickListener(v -> {
+            start();
+        });
+
+        binding.btnStop.setOnClickListener(v -> {
+            stop();
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stop();
+    }
+
+    private Handler mHandler = new Handler();
+    private int interval = 1000;
+
+    private void start() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "running...... ");
+                mHandler.postDelayed(this, interval);
+            }
+        }, interval);
+    }
+
+    private void stop() {
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
