57477415d18249889782adb3df028cd7f88704ac
LloydFinch <wangwenqi@orangelab.cn>
2020-03-20 18:21:08 +0800

    verify some way to obtain view measure size

--- app/src/main/java/com/lloydfinch/gradleforandroid/MainActivity.java
+++ app/src/main/java/com/lloydfinch/gradleforandroid/MainActivity.java
@@ -3,11 +3,16 @@ package com.lloydfinch.gradleforandroid;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.lloydfinch.gradleforandroid.databinding.ActivityMainBinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * @author lloydfinch
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -29,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
         binding.btnStop.setOnClickListener(v -> {
             stop();
         });
+
+        binding.btnStop.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        // testViewPost();
+        testViewTreeObserver();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume before: ");
+        printRootSize();
+        Log.e(TAG, "onResume after: ");
     }
 
     @Override
@@ -52,5 +75,65 @@ public class MainActivity extends AppCompatActivity {
 
     private void stop() {
         mHandler.removeCallbacksAndMessages(null);
+    }
+
+    /**
+     * 证明View.post()可以获取View的measure size
+     */
+    private void testViewPost() {
+        View root = binding.getRoot();
+        root.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "run: by root post! ");
+                printRootSize();
+            }
+        });
+
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(root);
+            }
+        }, 3000);
+    }
+
+    /**
+     * 这个用来证明OnGlobalLayoutListener里面可以获取到View的measure size
+     */
+    private void testViewTreeObserver() {
+        View root = binding.getRoot();
+        ViewTreeObserver observer = root.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Log.e(TAG, "onGlobalLayout before:");
+                printRootSize();
+                Log.e(TAG, "onGlobalLayout after:");
+            }
+        });
+    }
+
+    /**
+     * 证明onWindowFocueChanged可以获取View的measure size
+     *
+     * @param
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            Log.e(TAG, "onWindowFocusChanged before: ");
+            printRootSize();
+            Log.e(TAG, "onWindowFocusChanged before: ");
+        }
+    }
+
+    private void printRootSize() {
+        View root = binding.getRoot();
+        int width = root.getMeasuredWidth();
+        int height = root.getMeasuredHeight();
+        Log.e(TAG, "printRootSize: [width = " + width + ", height = " + height + "]");
     }
 }
