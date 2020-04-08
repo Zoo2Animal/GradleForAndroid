e9cd5d0f65923693da7f1a4c98331e4e8deffc91
LloydFinch <wangwenqi@orangelab.cn>
2020-04-08 18:40:41 +0800

    add runtask and run launcher activity dependson install task

--- javalib/src/main/java/com/lloydfinch/javalib/TestClass.java
+++ javalib/src/main/java/com/lloydfinch/javalib/TestClass.java
@@ -1,4 +1,27 @@
 package com.lloydfinch.javalib;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestClass {
+
+    /**
+     * 测试Properties，类似于Android的SharedPreferences
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        File file = new File("private.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(new DataInputStream(new FileInputStream(file)));
+            String password = properties.getProperty("release.password");
+            System.out.println("password: " + password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
